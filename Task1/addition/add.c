680d671d28620cf4ad509ef3fcb2d90b2103d3e8
Rayhan Ahmed Shourov <152443386+Rayhan0-1@users.noreply.github.com>
2025-05-23 21:02:58 +0600

    Delete Task1 directory

--- Task1/addition/add.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-    int x = 9, y = 3, add;
-    add = x+y;
-    printf("%d",add);
-
-    return 0;
-}
--- Task1/multiplication/mul.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-    int x = 9, y = 3, mul;
-    mul = x*y;
-    printf("%d",mul);
-
-    return 0;
-}
